a84441f7d30621fcb2a352815dff69133dc979d7
Steve Braich <33675736+steve3p0@users.noreply.github.com>
2020-04-06 13:46:10 -0700

    using temp files for .ps

--- tree_generator/tree_generator.py
+++ tree_generator/tree_generator.py
@@ -3,10 +3,6 @@ from nltk.parse import stanford
 from nltk.draw.tree import TreeView
 from PIL import Image
 
-os.environ['STANFORD_PARSER'] = 'C:\\workspace_courses\\LING511\\tree_generator'
-os.environ['STANFORD_MODELS'] = 'C:\\workspace_courses\\LING511\\tree_generator'
-model_path = "C:\\workspace_courses\\LING511\\tree_generator\\englishPCFG.ser.gz"
-
 # Requirements:
 # This script requires Ghostscript for image conversion
 # You have to install the python package (via pip):
@@ -19,51 +15,67 @@ model_path = "C:\\workspace_courses\\LING511\\tree_generator\\englishPCFG.ser.gz
 # Add that line to the path environment variable
 # You may have to reboot for the path environment variable to take effect
 
-
-def write_to_file(tree, filename):
-    if os.path.exists(f"{filename}.ps"):
-        os.remove(f"{filename}.ps")
-    if os.path.exists(f"{filename}.png"):
-        os.remove(f"{filename}.png")
-    TreeView(tree)._cframe.print_to_file(f"{filename}.ps")
-
-    with Image.open(f"{filename}.ps") as img:
-        img.load(scale=4)
-        img.save(f"{filename}.png")
-
-    try:
-        os.remove(f"{filename}.ps")
-    except OSError as e:  # name the Exception `e`
-        print("Failed with:", e.strerror)  # look what it says
+os.environ['STANFORD_PARSER'] = 'C:\\workspace_courses\\LING511\\tree_generator'
+os.environ['STANFORD_MODELS'] = 'C:\\workspace_courses\\LING511\\tree_generator'
+model_path = "C:\\workspace_courses\\LING511\\tree_generator\\englishPCFG.ser.gz"
 
 
-def parse_trees(sentences):
-    # Output trees to file
-    parser = stanford.StanfordParser(model_path=model_path)
-    trees = parser.raw_parse_sents(sentences)
+class TreeBuilder:
+    def __init__(self, parser):
+        self.parser = parser
 
-    i = 0
-    for line in trees:
-        for tree in line:
+    @staticmethod
+    def tree_to_string(tree):
+        s = str(tree)
+        return s
+
+    @staticmethod
+    def write_to_file(tree, filename):
+        if os.path.exists(f"{filename}.png"):
+            os.remove(f"{filename}.png")
+
+        import tempfile
+        with tempfile.NamedTemporaryFile() as file:
+            in_path = "{0:}.ps".format(file.name)
+            TreeView(tree)._cframe.print_to_file(in_path)
+            with Image.open(in_path) as img:
+                img.load(scale=4)
+                img.save(f"{filename}.png")
+
+    def parse_sentence(self, sentence):
+        # fromstring
+        #:type remove_empty_top_bracketing: bool
+        tree = next(self.parser.raw_parse(sentence))
+        return tree
+
+    def parse_sentences(self, sentences):
+        i = 0
+        for s in sentences:
             i += 1
             filename = f"tree_{i}"
-            write_to_file(tree, filename)
+            tree = self.parse_sentence(s)
+            tree_str = self.tree_to_string(tree)
+            print(tree_str)
+            self.write_to_file(tree, filename)
 
 
-sentences = [
-                "The animals did not think the buffalo would eat them",
-                "They were afraid the buffalo would trample them",
-                "The buffalo were pursuing fresh grass",
-                "Those buffalo were large and lumbering",
-                "The herd that the animals had heard caused considerable alarm",
-                "One young buffalo trotted slowly behind the herd",
-                "He was smelling the fresh grass",
-                "This buffalo was wondering whether he would find any adventures",
-                "He was tired of the dry grassy plains",
-                "He thought that other places must be more interesting"
-            ]
+if __name__ == '__main__':
+    # sentences = ["He thought that other places must be more interesting"]
 
-# sentences = ["He thought that other places must be more interesting"]
+    sentences = [
+                    "The animals did not think the buffalo would eat them",
+                    "They were afraid the buffalo would trample them",
+                    "The buffalo were pursuing fresh grass",
+                    "Those buffalo were large and lumbering",
+                    "The herd that the animals had heard caused considerable alarm",
+                    "One young buffalo trotted slowly behind the herd",
+                    "He was smelling the fresh grass",
+                    "This buffalo was wondering whether he would find any adventures",
+                    "He was tired of the dry grassy plains",
+                    "He thought that other places must be more interesting"
+                ]
 
-parse_trees(sentences)
+    parser = stanford.StanfordParser(model_path=model_path)
+    tree_builder = TreeBuilder(parser)
+    tree_builder.parse_sentences(sentences)
 
