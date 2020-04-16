93bada9384ecf6db7c41d50a50e2ad35016ea3c5
Steve Braich <33675736+steve3p0@users.noreply.github.com>
2020-04-16 09:46:11 -0700

    Task pane now will include parse string.

--- PsuSyntax_OneNoteAddIn/PSU Syntax OneNote Add-in/src/taskpane/taskpane.js
+++ PsuSyntax_OneNoteAddIn/PSU Syntax OneNote Add-in/src/taskpane/taskpane.js
@@ -5,36 +5,146 @@
 
 /* global document, Office */
 
-Office.onReady(info => {
-  if (info.host === Office.HostType.OneNote) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
-  }
+Office.onReady(info =>
+{
+    if (info.host === Office.HostType.OneNote)
+    {
+        document.getElementById("sideload-msg").style.display = "none";
+        document.getElementById("app-body").style.display = "flex";
+        document.getElementById("run").onclick = run;
+    }
 });
 
 export async function run() {
-  /**
-   * Insert your OneNote code here
-   */
+    /**
+     * Insert your OneNote code here
+     */
 
-  try {
-    await OneNote.run(async context => {
+    try
+    {
+        // eslint-disable-next-line no-undef
+        await OneNote.run(async context =>
+        {
 
-      // Get the current page.
-      var page = context.application.getActivePage();
+            // Get the current page.
+            var page = context.application.getActivePage();
 
-      // Queue a command to set the page title.
-      page.title = "Hello World";
+            // Queue a command to set the page title.
+            page.title = "STRING PARSED";
 
-      // Queue a command to add an outline to the page.
-      var html = "<p><ol><li>Item #1</li><li>Item #2</li></ol></p>";
-      page.addOutline(40, 90, html);
+            // // Queue a command to add an outline to the page.
+            // var html = "<p><ol><li>Item #1</li><li>Item #2</li></ol></p>";
+            // page.addOutline(40, 90, html);
 
-      // Run the queued commands, and return a promise to indicate task completion.
-      return context.sync();
-    });
-  } catch (error) {
-    console.log("Error: " + error);
-  }
+
+            Office.context.document.getSelectedDataAsync(Office.CoercionType.Text, function (asyncResult)
+            {
+                if (asyncResult.status == Office.AsyncResultStatus.Failed)
+                {
+                    write('Action failed. Error: ' + asyncResult.error.message);
+                }
+                else
+                {
+                    //write('Selected data: ' + asyncResult.value);
+
+                    var selectedText = asyncResult.value
+                    var parseStr = "";
+
+                    const host = "https://47abd88d.ngrok.io"
+                    const axios = require('axios').default;
+                    axios.post(host + '/parse',
+                    {
+                        sentence: selectedText
+                    })
+                    .then(function (response)
+                    {
+                        parseStr = response.data;
+                        // Create newText by appendening parse tree
+                        var newText = selectedText
+                            + "<br><br>"
+                            + "Parse String: <br>"
+                            + parseStr ;
+                        console.log("New text is: " + newText);
+
+                        // Replace selected text with newText value.
+                        Office.context.document.setSelectedDataAsync(newText, { coercionType: "html" },
+                            function (asyncResult) {
+                                var error = asyncResult.error;
+                                if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+                                    console.log(error.name + ": " + error.message);
+                                }
+                            });
+
+                        console.log(response);
+                    })
+                    .catch(function (error)
+                    {
+                        console.log(error);
+                    });
+
+
+                    // // Queue a command to add an outline to the page.
+                    // var html = "<p><ol>"
+                    //     + "<li>Sentence: " + selectedText + "</li>"
+                    //     + "<li>Parse Tree: " + parseStr + "</li></ol></p>";
+                    // page.addOutline(40, 90, html);
+
+                }
+            });
+
+            // Run the queued commands, and return a promise to indicate task completion.
+            return context.sync();
+        });
+    }
+    catch (error)
+    {
+        // eslint-disable-next-line no-undef
+        console.log("Error: " + error);
+    }
 }
+
+// function getSelectedText()
+// {
+//     var selected = ""
+//     Office.context.document.getSelectedDataAsync(Office.CoercionType.Text, function (asyncResult)
+//     {
+//         if (asyncResult.status == Office.AsyncResultStatus.Failed)
+//         {
+//             write('Action failed. Error: ' + asyncResult.error.message);
+//         }
+//         else
+//         {
+//             //write('Selected data: ' + asyncResult.value);
+//
+//             selected = asyncResult.value;
+//         }
+//     });
+//
+//     return selected;
+// }
+
+// Function that writes to a div with id='message' on the page.
+function write(message)
+{
+    document.getElementById('message').innerText += message;
+}
+
+
+// function getSelectedText() {
+//     Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+//         { valueFormat: "unformatted" },
+//         function (asyncResult) {
+//             var error = asyncResult.error;
+//             if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+//                 console.log(error.name + ": " + error.message);
+//             }
+//             else {
+//                 // Get selected data.
+//                 var dataValue = asyncResult.value;
+//                 console.log("Selected data is: " + dataValue);
+//
+//                 return dataValue;
+//             }
+//         });
+// }
+
