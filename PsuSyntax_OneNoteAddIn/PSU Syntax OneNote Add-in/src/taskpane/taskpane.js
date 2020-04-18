1feffc244528bfeb811fd774affd39b10a772a7d
Steve Braich <33675736+steve3p0@users.noreply.github.com>
2020-04-18 02:58:45 -0700

    Parse outputs formatted in an html table.

--- PsuSyntax_OneNoteAddIn/PSU Syntax OneNote Add-in/src/taskpane/taskpane.js
+++ PsuSyntax_OneNoteAddIn/PSU Syntax OneNote Add-in/src/taskpane/taskpane.js
@@ -5,7 +5,7 @@
 
 /* global document, Office */
 
-const HOST = "https://47abd88d.ngrok.io"
+const HOST = "https://7228f422.ngrok.io"
 
 Office.onReady(info =>
 {
@@ -34,57 +34,6 @@ export async function run() {
             // Queue a command to set the page title.
             page.title = "STRING PARSED";
 
-            // // Queue a command to add an outline to the page.
-            // var html = "<p><ol><li>Item #1</li><li>Item #2</li></ol></p>";
-            // page.addOutline(40, 90, html);
-            //const checkedOutputFormats = document.querySelector('#outputs input[type=checkbox]:checked").value;
-
-
-
-            // const ckb = document.querySelectorAll("#myDiv input[type=checkbox]");
-            //
-            // [...ckb].forEach( el => {
-            //
-            //     if( el.checked ) {
-            //         // Is checked!
-            //         console.log( el.value )
-            //         el.closest("label").style.background = "gold";
-            //     } else {
-            //         // Not checked one
-            //         // ... do something else
-            //         el.closest("label").style.background = "gray";
-            //     }
-            //
-            // });
-
-            //var blah = document.getElementById("treeImage").
-            // var parserType = document.querySelector('input[name="type"]:checked').value;
-            // var outputFormats = document.querySelector('input[name="output"]:checked').value;
-            //
-            // var tree_image = document.getElementById("tree_image").checked
-            //
-            // var tree_image = ""
-            // if (tree_image)
-            // {
-            //
-            // }
-
-            // var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-            // var checkedOne = Array.prototype.slice.call(checkboxes).some(x => x.checked);
-
-            // if (!checkedOne)
-            // {
-            //     //alert("Please check at least one output format.")
-            //     document.getElementById("outputFormats").style.borderColor = "#ff0000";
-            //
-            //     // document.getElementById("outputFormats").style.border="1px solid red";
-            //     //
-            //     // document.getElementById("fName").className = document.getElementById("fName").className + " error";  // this adds the error class
-            //     //
-            //     // document.getElementById("fName").className = document.getElementById("fName").className.replace(" error", "");
-            //
-            // }
-
             Office.context.document.getSelectedDataAsync(Office.CoercionType.Text, function (asyncResult)
             {
                 if (asyncResult.status == Office.AsyncResultStatus.Failed)
@@ -96,9 +45,8 @@ export async function run() {
                     //write('Selected data: ' + asyncResult.value);
 
                     var selectedText = asyncResult.value
-                    var parseStr = "";
 
-                    var outputs = []
+                    var request_formats = []
                     const ckb = document.querySelectorAll("#outputs input[type=checkbox]");
                     var checkedOne = Array.prototype.slice.call(ckb).some(x => x.checked);
 
@@ -113,7 +61,7 @@ export async function run() {
                         if( el.checked )
                         {
                             // Is checked!
-                            outputs.push(el.id)
+                            request_formats.push(el.id)
                             //data.append(el.name, True)
                             console.log( el.id )
                         }
@@ -122,49 +70,34 @@ export async function run() {
                     //const parserType = document.querySelector('input[name="type"]:checked').value;
                     const parserType = document.querySelector('#types input[name="parser_type"]:checked').value;
 
-                    //var data = {};
-
                     var data = new FormData();
                     data.append("sentence", selectedText);
                     data.append("parser", parserType);
-                    data.append("formats", JSON.stringify(outputs))
+                    data.append("request_formats", JSON.stringify(outputs))
 
-                    const HOST = "https://211777d5.ngrok.io"
                     const axios = require('axios').default;
-                    axios.post(HOST + '/parse', data)
+
+                    axios.post(HOST + '/parse',
+                    {
+                        sentence: selectedText,
+                        parser: parserType,
+                        request_formats: request_formats
+                    })
                     .then(function (response)
                     {
-                        parseStr = response.data;
-                        // Create newText by appendening parse tree
-                        var newText = selectedText
-                            + "<br><br>"
-                            + "Parse String: <br>"
-                            + parseStr ;
-                        console.log("New text is: " + newText);
-
-                        // Replace selected text with newText value.
-                        Office.context.document.setSelectedDataAsync(newText, { coercionType: "html" },
-                            function (asyncResult) {
-                                var error = asyncResult.error;
-                                if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-                                    console.log(error.name + ": " + error.message);
-                                }
-                            });
-
+                        const response_data = response.data;
                         console.log(response);
+
+                        var html = createHtmlParseTable(response_data);
+
+                        addHOutlineToPage(html);
+                        // Add an outline with the specified HTML to the page.
+                        var outline = page.addOutline(560, 70, html);
                     })
                     .catch(function (error)
                     {
                         console.log(error);
                     });
-
-
-                    // // Queue a command to add an outline to the page.
-                    // var html = "<p><ol>"
-                    //     + "<li>Sentence: " + selectedText + "</li>"
-                    //     + "<li>Parse Tree: " + parseStr + "</li></ol></p>";
-                    // page.addOutline(40, 90, html);
-
                 }
             });
 
@@ -179,48 +112,140 @@ export async function run() {
     }
 }
 
-// function getSelectedText()
-// {
-//     var selected = ""
-//     Office.context.document.getSelectedDataAsync(Office.CoercionType.Text, function (asyncResult)
-//     {
-//         if (asyncResult.status == Office.AsyncResultStatus.Failed)
-//         {
-//             write('Action failed. Error: ' + asyncResult.error.message);
-//         }
-//         else
-//         {
-//             //write('Selected data: ' + asyncResult.value);
-//
-//             selected = asyncResult.value;
-//         }
-//     });
-//
-//     return selected;
-// }
-
 // Function that writes to a div with id='message' on the page.
 function write(message)
 {
     document.getElementById('message').innerText += message;
 }
 
+function createHtmlParseTable(data)
+{
+    const sentence = data["sentence"];
+    const parser = data["parser"];
+    const formats = data["response_formats"];
 
-// function getSelectedText() {
-//     Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
-//         { valueFormat: "unformatted" },
-//         function (asyncResult) {
-//             var error = asyncResult.error;
-//             if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-//                 console.log(error.name + ": " + error.message);
-//             }
-//             else {
-//                 // Get selected data.
-//                 var dataValue = asyncResult.value;
-//                 console.log("Selected data is: " + dataValue);
-//
-//                 return dataValue;
-//             }
-//         });
-// }
+    const asciiTree = formats["tree_ascii"].replace(/\n/g, "<br />");
+    const bracketDiagram = formats["bracket_diagram"];
+    const parseStr = formats["tree_str"];
 
+    const html_asciiTree =
+        "<span style='font-family: Courier New'>"
+        + "<p>"
+        + asciiTree
+        + "</p>"
+        + " </span>";
+
+    // const html_asciiTree =
+    //     "<p>"
+    //     + response_asciiTree
+    //     + "</p>";
+
+    // //var data = {};
+    // var table = document.createElement(OneNote.Table)
+    //
+    // // table.appendRow("Syntax Tree (Image)")
+    // // table.appendRow("")
+    // table.appendRow("Syntax Tree (ASCII)")
+    // table.appendRow("<span style='font-family: Courier New'>" + response_asciiTree + "</span>")
+    // table.appendRow("Bracket Diagram:")
+    // table.appendRow(response_bracketDiagram)
+    // table.appendRow("Parse String:")
+    // table.appendRow(response_parseStr)
+
+    // var appBody = document.getElementById("app-body")
+    // appBody.append(
+    //     document.createElement(OneNote.Table)
+    //         .appendRow("Syntax Tree (ASCII)")
+    //         .appendRow("<span style='font-family: Courier New'>" + response_asciiTree + "</span>")
+    //         .appendRow("Bracket Diagram:")
+    //         .appendRow(response_bracketDiagram)
+    //         .appendRow("Parse String:")
+    //         .appendRow(response_parseStr)
+    // )
+
+    // var row  =  document.createElement(OneNote.TableRow)
+    // var cell =  document.createElement(OneNote.TableCell)
+
+    // const html_asciiTree =
+    //     "<span style='font-family: Courier New'>"
+    //     + response_asciiTree
+    //     + " </span>";
+
+    // // Create newText by appendening parse tree
+    // var newText = selectedText
+    //     + "<br><br>"
+    //     + "Syntax Tree (ASCII): <br>" + html_asciiTree + "<br>"
+    //     + "Bracket Diagram: <br>" + response_bracketDiagram + "<br>"
+    //     + "Parse String: <br>" + response_parseStr + "<br>";
+
+    //var onTable = new OneNote.Table()
+
+
+    // Create newText by appendening parse tree
+    var html =
+        // CSS Note working?
+        "<table border=1>"
+        //+ "<table class='pdx-ling_parseTable'>"
+        //+ "<tr bgcolor='#d3d3d3'><td>"
+        //+ "<tr style='background-color: lightgray'><td>"
+        //+ "<tr><td>"
+        //+ "<tr bgcolor='#d3d3d3'><td>"
+        //+ "<tr style='background-color: lightgray'><td>"
+        //+ "<tr><td bgcolor='#d3d3d3'>"
+        + "<tr><td style='background-color: lightgray'>"
+        + "Syntax Tree (ASCII):"
+        + "</td></tr>"
+        + "<tr><td>"
+        +  html_asciiTree
+        + "</td></tr>"
+        + "<tr bgcolor='#d3d3d3'><td>"
+        + "Bracket Diagram:"
+        + "</td></tr>"
+        + "<tr><td>"
+        + bracketDiagram
+        + "</td></tr>"
+        + "<tr bgcolor='#d3d3d3'><td>"
+        + "Parse String:"
+        + "</td></tr>"
+        + "<tr><td>"
+        + parseStr
+        + "</td></tr>"
+        + "</table>"
+
+    // // Create newText by appendening parse tree
+    // var newText = selectedText + table
+
+
+    // // Create newText by appendening parse tree
+    // var newText = selectedText
+    // console.log("New text is: " + newText);
+    //
+    // // Replace selected text with newText value.
+    // Office.context.document.setSelectedDataAsync(newText, { coercionType: "html" },
+    //     function (asyncResult) {
+    //         var error = asyncResult.error;
+    //         if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+    //             console.log(error.name + ": " + error.message);
+    //         }
+    //     });
+
+    return html
+}
+
+// Add a table that displays the final grade, individual scores, and comments to the page.
+function addHOutlineToPage(html) {
+    OneNote.run(function (context) {
+
+        // Get the current page.
+        var page = context.application.getActivePage();
+
+        // Add an outline with the specified HTML to the page.
+        var outline = page.addOutline(560, 70, html);
+
+        // Run the queued commands, and return a promise to indicate task completion.
+        return context.sync()
+            .catch(function(error) {
+                onError(error);
+            });
+    });
+}
